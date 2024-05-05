416fa7a9a0e95b326889c03fb38840ebaf326d18
ConfuzzedCat <confuzzedcat@gmail.com>
2024-05-05 23:37:42 +0200

    Added functionality to edit fields

--- src/components/KanBanCard.vue
+++ src/components/KanBanCard.vue
@@ -3,16 +3,70 @@
 </style>
 
 <script setup>
-const props = defineProps(['title', 'desc'])
+import { ref } from "vue"
+let props = defineProps(['title', 'desc'])
+const isTitleEditing = ref(false)
+const isDescEditing = ref(false)
+
 
 console.log("Cards Props", props.title, props.desc)
+
+function titleEditToggle(){
+    isTitleEditing = !isTitleEditing
+    console.log("isTitleEditing:", isTitleEditing)
+}
+
+function descEditToggle(){
+    isDescEditing = !isDescEditing
+}
+
+function updateTitle(s){
+    let tprops = props
+    props = {
+        title: s,
+        desc: tprops.desc
+    }
+    console.log(props)
+}
+
+function updateDesc(s){
+    let tprops = props
+    props = {
+        title: tprops.title,
+        desc: s
+    }
+    console.log(props)
+}
 </script>
 
 <template>
     <div class="border-padding">
-        <v-card                        
-            :title="props.title"
-            :text="props.desc"
-        ></v-card>
+
+        <v-card>
+        <v-card-item> 
+            <v-card-title >
+                <v-text-field 
+                    @update:model-value="updateTitle" 
+                    @click:append="isTitleEditing = !isTitleEditing" 
+                    :label="props.title" 
+                    append-icon="mdi-check-circle" 
+                    v-if="isTitleEditing"></v-text-field>
+                <p v-else 
+                    @click="isTitleEditing = !isTitleEditing"
+                >{{ props.title }}</p>
+            </v-card-title>
+        </v-card-item>
+        <v-card-text>
+            <v-text-field 
+                    @update:model-value="updateDesc" 
+                    @click:append="isDescEditing = !isDescEditing" 
+                    :label="props.desc" 
+                    append-icon="mdi-check-circle" 
+                    v-if="isDescEditing"></v-text-field>
+                <p v-else 
+                    @click="isDescEditing = !isDescEditing"
+                >{{ props.desc }}</p>
+        </v-card-text>
+      </v-card>
     </div>
 </template>
